9ec41e977c864cd43d56deeef530eca6de0289d2
Gianni Genovesi <gg@customerly.io>
2019-10-25 19:25:16 +0200

    fix - fixed sound now played on notification stream - added optional callback to logoutUser - release 3.0.3

--- customerly-android-sdk/src/main/java/io/customerly/utils/Sound.kt
+++ customerly-android-sdk/src/main/java/io/customerly/utils/Sound.kt
@@ -1,7 +1,10 @@
 package io.customerly.utils
 
 import android.content.Context
+import android.media.AudioAttributes
+import android.media.AudioManager
 import android.media.MediaPlayer
+import android.os.Build
 import io.customerly.R
 
 /**
@@ -9,10 +12,24 @@ import io.customerly.R
  * Project: DemoCustomerly
  */
 internal fun Context.playNotifSound() {
-    MediaPlayer.create(this, R.raw.io_customerly__notif_2).apply {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        MediaPlayer.create(this,
+                R.raw.io_customerly__notif_2,
+                AudioAttributes
+                        .Builder()
+                        .setLegacyStreamType(AudioManager.STREAM_NOTIFICATION)
+                        .build(),
+                (this.getSystemService(Context.AUDIO_SERVICE) as AudioManager).generateAudioSessionId())
+    } else {
+        MediaPlayer.create(this, R.raw.io_customerly__notif_2).apply {
+            @Suppress("DEPRECATION")
+            this.setAudioStreamType(AudioManager.STREAM_NOTIFICATION)
+        }
+    }.apply {
         this.setOnCompletionListener { mp ->
             mp.reset()
             mp.release()
         }
-    }.start()
+        this.start()
+    }
 }
